6cf6c4373f4a048ba506167c16631b7c3447d963
IsaboevSanjar <sanjarisaboev@yandex.com>
2023-09-12 13:40:09 +0500

    entity is done
    Signed-off-by: SanjarIsaboev <sanjarisaboev@yandex.com>

---
+++ app/src/main/java/sanjar/uz/roomwordexample/Word.kt
@@ -0,0 +1,12 @@
+package sanjar.uz.roomwordexample
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "world_table")
+data class Word(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "word")
+    val word: String
+)
